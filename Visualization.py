ed342f2e34b8cdf0851419289445a522cd8a39c4
andruhus <aaademed@gmail.com>
2021-03-28 15:48:00 +0300

    Added two more input boxes

--- Visualization.py
+++ Visualization.py
@@ -4,7 +4,66 @@ import pygame as pg
 def main():
     screen = pg.display.set_mode((640, 480))
     font = pg.font.Font(None, 32)
-    clock = pg.time.Clock()
+    input_boxes = [
+        pg.Rect(100, 100, 140, 32),
+        pg.Rect(100, 150, 140, 32),
+        pg.Rect(100, 200, 140, 32)
+    ]
+    color_inactive = pg.Color('lightskyblue3')
+    color_active = pg.Color('dodgerblue2')
+    color = [
+        color_inactive,
+        color_inactive,
+        color_inactive
+    ]
+    active = [False,False,False]
+    text = ['','','']
+    done = False
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+            if event.type == pg.MOUSEBUTTONDOWN:
+                for index in range(3):
+                    if input_boxes[index].collidepoint(event.pos):
+                        active[index] = not active[index]
+                    else:
+                        active[index] = False
+                    color[index] = color_active if active[index] else color_inactive
+            if event.type == pg.KEYDOWN:
+                for index in range(3):
+                    if active[index]:
+                        if event.key == pg.K_RETURN:
+                            print(text[index])
+                            text[index] = ''
+                        elif event.key == pg.K_BACKSPACE:
+                            text[index] = text[index][:-1]
+                        else:
+                            text[index] += event.unicode
+        screen.fill((30, 30, 30))
+        # Render the current text.
+        txt_surface = []
+        for index in range(3):
+            txt_surface.append( font.render(text[index], True, color[index]))
+        # Resize the box if the text is too long.
+        for index in range(3):
+            width = max(200, txt_surface[index].get_width() + 10)
+            input_boxes[index].w = width
+        # Blit the text.
+            screen.blit(txt_surface[index], (input_boxes[index].x + 5, input_boxes[index].y + 5))
+        # Blit the input_box rect.
+            pg.draw.rect(screen, color[index], input_boxes[index], 2)
+
+        pg.display.flip()
+
+pg.init()
+main()
+pg.quit()
+"""
+def main():
+    screen = pg.display.set_mode((640, 480))
+    font = pg.font.Font(None, 32)
+    #clock = pg.time.Clock()
     input_box = pg.Rect(100, 100, 140, 32)
     color_inactive = pg.Color('lightskyblue3')
     color_active = pg.Color('dodgerblue2')
@@ -48,10 +107,11 @@ def main():
         pg.draw.rect(screen, color, input_box, 2)
 
         pg.display.flip()
-        clock.tick(30)
+        #clock.tick(30)
         pass
 
 
 pg.init()
 main()
 pg.quit()
+"""
