5e82a88c3a7777d935ad39ed4cd2d5962cf3c388
IhorTarkhan <ihor.lafox.net>
2021-03-28 19:56:39 +0300

    memory using tracing

--- main_factory_test.py
+++ main_factory_test.py
@@ -4,20 +4,19 @@ from sort_utils.algoritms.CocktailSorter import CocktailSorter
 from sort_utils.algoritms.MargeSorter import MargeSorter
 from sort_utils.algoritms.QuickSorter import QuickSorter
 from sort_utils.algoritms.RadixSorter import RadixSorter
-from time_utils.TimeUtil import get_in_milliseconds
 
 array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-brick_time = BrickSorter().benchmark(array).time
-bubble_time = BubbleSorter().benchmark(array).time
-cocktail_time = CocktailSorter().benchmark(array).time
-marge_time = MargeSorter().benchmark(array).time
-quick_time = QuickSorter().benchmark(array).time
-radix_time = RadixSorter().benchmark(array).time
+brick = BrickSorter().benchmark(array)
+bubble = BubbleSorter().benchmark(array)
+cocktail = CocktailSorter().benchmark(array)
+marge = MargeSorter().benchmark(array)
+quick = QuickSorter().benchmark(array)
+radix = RadixSorter().benchmark(array)
 
-print('brick_time', get_in_milliseconds(brick_time))
-print('bubble_time', get_in_milliseconds(bubble_time))
-print('cocktail_time', get_in_milliseconds(cocktail_time))
-print('marge_time', get_in_milliseconds(marge_time))
-print('quick_time', get_in_milliseconds(quick_time))
-print('radix_time', get_in_milliseconds(radix_time))
+print('brick', brick)
+print('bubble', bubble)
+print('cocktail', cocktail)
+print('marge', marge)
+print('quick', quick)
+print('radix', radix)
--- sort_utils/SorterResult.py
+++ sort_utils/SorterResult.py
@@ -1,4 +1,10 @@
+from float_utils.FloatUtil import get_pretty
+
+
 class SorterResult:
-    def __init__(self, time: float, memory_used: float):
-        self.time = time
+    def __init__(self, time_used: float, memory_used: float):
+        self.time_used = time_used
         self.memory_used = memory_used
+
+    def __str__(self):
+        return f'[{get_pretty(self.time_used * 1000)} ms, {get_pretty(self.memory_used)} cu]'
