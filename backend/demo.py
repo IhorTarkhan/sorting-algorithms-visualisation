40a851d3676a148a332396e66430b8e8f036a551
IhorTarkhan <ihor.lafox.net>
2021-04-04 19:52:00 +0300

    main optimised

--- backend/demo.py
+++ backend/demo.py
@@ -1,11 +1,7 @@
 from backend.controller.benchmark_run import run_benchmark
-from backend.db.sorting_logs_connector import \
-    create_table_if_not_exist
+from backend.db.sorting_logs_connector import create_table_if_not_exist
 from entity.ArrayOrderEnum import ArrayOrderEnum
 
 if __import__('__main__'):
     create_table_if_not_exist()
-    for size in range(1, 1001):
-        for iteration in range(100):
-            print('size={};\t\titeration={}'.format(size, iteration))
-            run_benchmark(1, 100, size, ArrayOrderEnum.RANDOM)
+    run_benchmark(1, 100, 100, ArrayOrderEnum.RANDOM)
