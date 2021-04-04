57c5df25b16b461f2f5a4976d853ac60faaeddc1
IhorTarkhan <ihor.lafox.net>
2021-04-04 13:31:40 +0300

    My sql in class wrap

--- backend/db/mysql_db.py
+++ backend/db/mysql_db.py
@@ -3,16 +3,18 @@ import sys
 
 import mysql.connector
 
-config = configparser.ConfigParser()
-config.read(sys.argv[1])
-mysql_config = config['mysql']
 
+class MySqlDb:
+    config = configparser.ConfigParser()
+    config.read(sys.argv[1])
+    mysql_config = config['mysql']
 
-def execute(sql: str):
-    database = mysql.connector.connect(**mysql_config)
-    cursor = database.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    cursor.close()
-    database.close()
-    return result
+    @classmethod
+    def execute(cls, sql: str):
+        database = mysql.connector.connect(**cls.mysql_config)
+        cursor = database.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        cursor.close()
+        database.close()
+        return result
