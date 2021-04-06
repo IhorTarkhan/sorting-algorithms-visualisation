4188f6a37743f568901a2f7f715aae0aa39abd7a
andruhus <aaademed@gmail.com>
2021-04-06 19:28:44 +0300

    bugfix3

--- UI/Extract_results.py
+++ UI/Extract_results.py
@@ -4,9 +4,9 @@ from entity.ArrayOrderEnum import ArrayOrderEnum
 
 class Extract_Results:
     def get_results(self, inputs, active):
-        self.min = int(inputs[0])
-        self.max = int(inputs[1])
-        self.quantity = int(inputs[2])
+        self.min = int(inputs[0]) if len(inputs[0]) > 0 else 0
+        self.max = int(inputs[1]) if len(inputs[1]) > 0 else 0
+        self.quantity = int(inputs[2]) if len(inputs[2]) > 0 else 0
         if active[0]:
             self.order = ArrayOrderEnum.RANDOM
         elif active[1]:
--- UI/Graphs.py
+++ UI/Graphs.py
@@ -43,23 +43,37 @@ class Graphs:
     def draw_a_line(self, x, y, y_label):
         plt.plot(x, y, y_label)
 
-    def split_by_algorythms(self, statistic):
+    def split_by_algorythms(self, statistic, index):
+        def get_proper_field(object, index):
+            if index == 0:
+                return object.ascending_time
+            if index == 1:
+                return object.random_time
+            if index == 2:
+                return object.descending_time
+            if index == 3:
+                return object.ascending_size
+            if index == 4:
+                return object.random_size
+            else:
+                return object.descending_size
+
         res = []
         for alg in range(len(self.labels)):
             res.append([])
         for item in statistic:
             if item.algorithm == SortAlgorithmEnum.BRICK:
-                res[0].append(item)
+                res[0].append((get_proper_field(item, index), item.array_size))
             elif item.algorithm == SortAlgorithmEnum.BUBBLE:
-                res[1].append(item)
+                res[1].append((get_proper_field(item, index), item.array_size))
             elif item.algorithm == SortAlgorithmEnum.COCKTAIL:
-                res[2].append(item)
+                res[2].append((get_proper_field(item, index), item.array_size))
             elif item.algorithm == SortAlgorithmEnum.MERGE:
-                res[3].append(item)
+                res[3].append((get_proper_field(item, index), item.array_size))
             elif item.algorithm == SortAlgorithmEnum.QUICK:
-                res[4].append(item)
+                res[4].append((get_proper_field(item, index), item.array_size))
             else:
-                res[5].append(item)
+                res[5].append((get_proper_field(item, index), item.array_size))
         return res
 
     def split_by_fields(self, statistic):
@@ -71,17 +85,17 @@ class Graphs:
         desc_m = []
         for item in statistic:
             if item.ascending_time != None:
-                asc_t.append((item.ascending_time, item.array_size))
+                asc_t.append(item)
             if item.random_time != None:
-                rand_t.append((item.random_time, item.array_size))
+                rand_t.append(item)
             if item.descending_time != None:
-                desc_t.append((item.descending_time, item.array_size))
+                desc_t.append(item)
             if item.ascending_size != None:
-                asc_m.append((item.ascending_size, item.array_size))
+                asc_m.append(item)
             if item.random_size != None:
-                rand_m.append((item.random_size, item.array_size))
+                rand_m.append(item)
             if item.descending_size != None:
-                desc_m.append((item.descending_size, item.array_size))
+                desc_m.append(item)
 
         return asc_t, rand_t, desc_t, asc_m, rand_m, desc_m
 
@@ -98,7 +112,7 @@ class Graphs:
         for graph_index in range(len(graphs)):
             graphs_separated = self.split_by_algorythms(graphs[graph_index])
             for alg_index in range(len(graphs_separated)):
-                x, y = self.get_xy_array(graphs_separated[alg_graph])
+                x, y = self.get_xy_array(graphs_separated[alg_index])
                 self.draw_a_line(x, y, self.colors[alg_index])
             plt.show()
 
