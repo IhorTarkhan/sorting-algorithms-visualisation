55eaca3fa18fb761e329a5531bf5beb3f5f8d72a
IhorTarkhan <ihor.lafox.net>
2021-04-07 08:30:53 +0300

    demo populate db

--- calculation/demo.py
+++ calculation/demo.py
@@ -1,5 +1,6 @@
-from calculation.controller.benchmark import run_benchmark, get_statistic
-from calculation.db.sorting_logs_connector import create_table_if_not_exist
+from controller.benchmark import run_benchmark
+from db.sorting_logs_connector import create_table_if_not_exist
+
 from entity.ArrayOrderEnum import ArrayOrderEnum
 
 if __import__('__main__'):
@@ -7,8 +8,10 @@ if __import__('__main__'):
         Function to demonstrate how to use controllers
     """
     create_table_if_not_exist()
-
-    run_benchmark(1, 100, 100, ArrayOrderEnum.RANDOM)
-
-    statistic_list = get_statistic()
-    print(len(statistic_list))
+    for i in range(10):
+        print('{}%'.format(i * 10))
+        for size in range(1, 101):
+            run_benchmark(1, 100, size, ArrayOrderEnum.ASC)
+            run_benchmark(1, 100, size, ArrayOrderEnum.RANDOM)
+            run_benchmark(1, 100, size, ArrayOrderEnum.DESC)
+    print('100%')
