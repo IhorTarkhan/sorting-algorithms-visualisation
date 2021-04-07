6b2fc03606b2b4625e84bc28729709e4280d663e
IhorTarkhan <ihor.lafox.net>
2021-04-07 09:10:26 +0300

    merge sort fix

--- calculation/service/sorting/algoritms/QuickSorter.py
+++ calculation/service/sorting/algoritms/QuickSorter.py
@@ -1,3 +1,5 @@
+import sys
+
 from calculation.service.sorting.algoritms.AbstractSorter import AbstractSorter
 from calculation.util.PySorter import PySorter
 
@@ -12,5 +14,10 @@ class QuickSorter(AbstractSorter):
         Implementation hide implementation and adding benchmark functionality
     """
 
+    @staticmethod
+    def sort_func(array):
+        sys.setrecursionlimit(len(array) + 100)
+        PySorter.get_instance().quickSort(array, 0, len(array) - 1)
+
     def __init__(self):
-        super().__init__(lambda array: PySorter.get_instance().quickSort(array, 0, len(array) - 1))
+        super().__init__(self.sort_func)
